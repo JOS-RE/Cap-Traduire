5b5e9c702091a51950a1464705d90575857d90e8
JOS-RE <joshithreddy.g@gmail.com>
2021-09-12 04:10:31 +0530

    changed the GUI interface, added end button

--- translator.py
+++ translator.py
@@ -12,6 +12,7 @@ import azure.cognitiveservices.speech as speech_sdk
 global targetLanguage
 targetLanguage = ''
 
+
 def gui():
 
     # from tkinter import *
@@ -23,33 +24,26 @@ def gui():
     def relative_to_assets(path: str) -> Path:
         return ASSETS_PATH / Path(path)
 
+
     window = Tk()
 
-    window.geometry("684x384")
-    window.configure(bg="#FFFFFF")
+    window.geometry("843x491")
+    window.configure(bg="#FE724D")
+
 
     canvas = Canvas(
         window,
-        bg="#FFFFFF",
-        height=384,
-        width=684,
+        bg="#FE724D",
+        height=491,
+        width=843,
         bd=0,
         highlightthickness=0,
         relief="ridge"
     )
 
     canvas.place(x=0, y=0)
-    canvas.create_rectangle(
-        0.0,
-        0.0,
-        684.0,
-        384.0,
-        fill="#FFEEEE",
-        outline="")
-
     button_image_1 = PhotoImage(
         file=relative_to_assets("button_1.png"))
-
     button_1 = Button(
         image=button_image_1,
         borderwidth=0,
@@ -58,15 +52,14 @@ def gui():
         relief="flat"
     )
     button_1.place(
-        x=362.00000000000006,
-        y=53.0,
-        width=287.0,
-        height=55.0
+        x=591.6849365234375,
+        y=53.99999999999994,
+        width=214.3150634765625,
+        height=186.14793395996094
     )
 
     button_image_2 = PhotoImage(
         file=relative_to_assets("button_2.png"))
-
     button_2 = Button(
         image=button_image_2,
         borderwidth=0,
@@ -74,17 +67,15 @@ def gui():
         command=lambda: makeES(targetLanguage),
         relief="flat"
     )
-
     button_2.place(
-        x=362.00000000000006,
-        y=201.0,
-        width=287.0,
-        height=55.0
+        x=359.0,
+        y=53.99999999999994,
+        width=214.3150634765625,
+        height=186.14793395996094
     )
 
     button_image_3 = PhotoImage(
         file=relative_to_assets("button_3.png"))
-
     button_3 = Button(
         image=button_image_3,
         borderwidth=0,
@@ -93,16 +84,15 @@ def gui():
         relief="flat"
     )
     button_3.place(
-        x=362.00000000000006,
-        y=275.0,
-        width=287.0,
-        height=55.0
+        x=359.0,
+        y=257.2931518554687,
+        width=214.3150634765625,
+        height=186.14793395996094
     )
 
-    button_image_4 = PhotoImage(
+    button_image_4=PhotoImage(
         file=relative_to_assets("button_4.png"))
-
-    button_4 = Button(
+    button_4=Button(
         image=button_image_4,
         borderwidth=0,
         highlightthickness=0,
@@ -110,17 +100,23 @@ def gui():
         relief="flat"
     )
     button_4.place(
-        x=362.00000000000006,
-        y=127.0,
-        width=287.0,
-        height=55.0
+        x=591.6849365234375,
+        y=257.2931518554687,
+        width=214.3150634765625,
+        height=186.14793395996094
     )
 
-    button_image_5 = PhotoImage(
-        file=relative_to_assets("button_5.png"))
+    canvas.create_rectangle(
+        37.0,
+        40.99999999999994,
+        306.0,
+        442.99999999999994,
+        fill="#FFFFFF",
+        outline="")
 
-    button_5 = Button(
-        text="abcd",
+    button_image_5=PhotoImage(
+        file=relative_to_assets("button_5.png"))
+    button_5=Button(
         image=button_image_5,
         borderwidth=0,
         highlightthickness=0,
@@ -128,49 +124,14 @@ def gui():
         relief="flat"
     )
     button_5.place(
-        x=30.000000000000057,
-        y=67.0,
-        width=249.0,
-        height=249.0
-    )
-
-    canvas.create_text(
-        411.00000000000006,
-        53.0,
-        anchor="nw",
-        text="Francais",
-        fill="#FFFFFF",
-        font=("VarelaRound Regular", 47 * -1)
-    )
-
-    canvas.create_text(
-        452.00000000000006,
-        276.0,
-        anchor="nw",
-        text="Hindi",
-        fill="#FFFFFF",
-        font=("VarelaRound Regular", 43 * -1)
-    )
-
-    canvas.create_text(
-        411.00000000000006,
-        201.0,
-        anchor="nw",
-        text="German",
-        fill="#FFFFFF",
-        font=("VarelaRound Regular", 42 * -1)
-    )
-
-    canvas.create_text(
-        411.00000000000006,
-        129.0,
-        anchor="nw",
-        text="Espanol",
-        fill="#FFFFFF",
-        font=("VarelaRound Regular", 42 * -1)
+        x=57.0,
+        y=382.99999999999994,
+        width=230.0,
+        height=39.0
     )
     window.resizable(False, False)
     window.mainloop()
+
 
 
 def main(targetLanguage):
@@ -180,13 +141,13 @@ def main(targetLanguage):
 
         # Get Configuration Settings
         load_dotenv()
-        cog_key = os.getenv('COG_SERVICE_KEY')
-        cog_region = os.getenv('COG_SERVICE_REGION')
+        cog_key=os.getenv('COG_SERVICE_KEY')
+        cog_region=os.getenv('COG_SERVICE_REGION')
 
         # Configure translation
-        translation_config = speech_sdk.translation.SpeechTranslationConfig(
+        translation_config=speech_sdk.translation.SpeechTranslationConfig(
             cog_key, cog_region)
-        translation_config.speech_recognition_language = 'en-US'
+        translation_config.speech_recognition_language='en-US'
         translation_config.add_target_language('fr')
         translation_config.add_target_language('es')
         translation_config.add_target_language('hi')
@@ -195,36 +156,36 @@ def main(targetLanguage):
               translation_config.speech_recognition_language)
 
         # Configure speech
-        speech_config = speech_sdk.SpeechConfig(cog_key, cog_region)
+        speech_config=speech_sdk.SpeechConfig(cog_key, cog_region)
 
         # Get user input
-        targetLanguag = ''
+        targetLanguag=''
         while targetLanguag != 'quit':
-            targetLanguag = gui()
+            targetLanguag=gui()
 
     except Exception as ex:
         print(ex)
 
 def makeFR(targetLanguage):
-    targetLanguage = "fr"
+    targetLanguage="fr"
     Translate(targetLanguage)
     return targetLanguage
 
 
 def makeES(targetLanguage):
-    targetLanguage = "es"
+    targetLanguage="es"
     Translate(targetLanguage)
     return targetLanguage
 
 
 def makeHI(targetLanguage):
-    targetLanguage = "hi"
+    targetLanguage="hi"
     Translate(targetLanguage)
     return targetLanguage
 
 
 def makeDE(targetLanguage):
-    targetLanguage = "de"
+    targetLanguage="de"
     Translate(targetLanguage)
     return targetLanguage
 
@@ -233,28 +194,28 @@ def makeqt(targetLanguage):
     return exit()
 
 def Translate(targetLanguage):
-    translation = ''
+    translation=''
 
     # Translate speech
-    audio_config = speech_sdk.AudioConfig(use_default_microphone=True)
-    translator = speech_sdk.translation.TranslationRecognizer(
+    audio_config=speech_sdk.AudioConfig(use_default_microphone=True)
+    translator=speech_sdk.translation.TranslationRecognizer(
         translation_config, audio_config)
     print("Speak now...")
-    result = translator.recognize_once_async().get()
+    result=translator.recognize_once_async().get()
     print(f'Translating {result.text}')
-    translation = result.translations[targetLanguage]
+    translation=result.translations[targetLanguage]
     print(translation)
 
     # Synthesize translation
-    voices = {
+    voices={
         "fr": "fr-FR-Julie",
         "es": "es-ES-Laura",
         "hi": "hi-IN-Kalpana",
         "de": "de-DE-HeddaRUS"
     }
-    speech_config.speech_synthesis_voice_name = voices.get(targetLanguage)
-    speech_synthesizer = speech_sdk.SpeechSynthesizer(speech_config)
-    speak = speech_synthesizer.speak_text_async(translation).get()
+    speech_config.speech_synthesis_voice_name=voices.get(targetLanguage)
+    speech_synthesizer=speech_sdk.SpeechSynthesizer(speech_config)
+    speak=speech_synthesizer.speak_text_async(translation).get()
     if speak.reason != speech_sdk.ResultReason.SynthesizingAudioCompleted:
         print(speak.reason)
 
